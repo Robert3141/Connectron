00b57455c34b67ead71c3b24b381e549a17fe60b
Robert3141 <robert.aries1@gmail.com>
2020-11-07 16:46:55 +0000

    V3.2.2

--- build.bat
+++ build.bat
@@ -4,9 +4,11 @@ flutter build appbundle && echo - ^
  && echo APK BUILD DONE && echo - ^
  && flutter build web && echo - ^
  && echo WEB BUILD DONE && echo - ^
- && cd ./build/web && del /f web.zip && 7z a -tzip web.zip * && copy web.zip "../app/outputs/apk/release" && cd ../../ && echo - ^
+ && cd ./build/web && del /f web.zip && "C:\Program Files\7-Zip\7z.exe" a -tzip web.zip * && copy web.zip "../app/outputs/apk/release" && cd ../../ && echo - ^
  && echo WEB ZIPPED && echo - ^
  && flutter build windows && echo - ^
  && echo WINDOWS BUILD DONE && echo - ^
- && cd ./build/windows && del /f windows.zip && 7z a -tzip windows.zip * && copy windows.zip "../app/outputs/apk/release" && cd ../../ && echo - ^
- && echo WINDOWS ZIPPED && echo -
+ && cd ./build/windows && del /f windows.zip && "C:\Program Files\7-Zip\7z.exe" a -tzip windows.zip * && copy windows.zip "../app/outputs/apk/release" && cd ../../ && echo - ^
+ && echo WINDOWS ZIPPED && echo - ^
+ && del /f "build/app/outputs/apk/release/output.json" && echo - ^
+ && echo CLEANED RELEASE FOLDER && echo -
